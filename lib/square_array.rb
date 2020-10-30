5702a069c90c90ae6faa74346d4c432166ad6861
Adri Torres <adriana.amaral@msn.com>
2020-10-30 18:59:18 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,14 @@
-def square_array(array)
-  # your code here
+
+
+ def square_array(array)
+
+ new_array = []
+counter = 0 
+while counter < array.length
+array[counter]
+new_array << array[counter] ** 2 
+counter += 1 
+end 
+
+ return new_array
 end
